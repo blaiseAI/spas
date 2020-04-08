6f367dd1a8195293bef7c66bf6bdef9f3588a14a
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-04-08 04:03:39 -0600

    Added a firebase connection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <template>
+    <div class="mt-3">
+        <div class="text-2xl text-center">Welcome Back <span class="text-indigo-800 font-extrabold">{{user}}</span></div>
         <div class="container mx-auto">
             <div class="px-4">
                 <div class="max-w-3xl bg-white rounded-lg mx-auto my-16 p-16">
@@ -6,12 +8,27 @@
                     <h2 class="font-medium text-sm text-indigo-400 mb-4 uppercase tracking-wide">Vue Firebase</h2>
                     <p>This simple app creates meetings, allow people to check in, and picks random users to award giveaways.</p>
                 </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import db from '../db.js'
 export default {
-  name: 'home'
+  name: 'home',
+  data: function(){
+      return{
+          user: null
+      }
+  },
+  mounted(){
+      db.collection("users")
+      .doc("uIVgChm4DsIqGgjx1oB4")
+      .get()
+      .then(snapshot => {
+          this.user = snapshot.data().name;
+      })
+  }
 }
 </script>
