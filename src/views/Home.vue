343218a7516500298885f56edd1b50f11f7de5b8
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-04-08 03:43:51 -0600

    update the title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,9 @@
         <div class="container mx-auto">
             <div class="px-4">
                 <div class="max-w-3xl bg-white rounded-lg mx-auto my-16 p-16">
-                    <h1 class="text-2xl font-medium mb-2">Let's Build: A vue single application with Tailwind css</h1>
-                    <h2 class="font-medium text-sm text-indigo-400 mb-4 uppercase tracking-wide">Responsive Navbar</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptatum cumque excepturi? Nemo, architecto! Illo tenetur, quae dolor ex quidem officiis dolore, quas, consectetur labore qui blanditiis consequatur harum illum.</p>
+                    <h1 class="text-2xl font-medium mb-2">Let's Build: A Meeting Log</h1>
+                    <h2 class="font-medium text-sm text-indigo-400 mb-4 uppercase tracking-wide">Vue Firebase</h2>
+                    <p>This simple app creates meetings, allow people to check in, and picks random users to award giveaways.</p>
                 </div>
         </div>
     </div>
