245f5fdbadef2567270d1d694eab7c18e9a05718
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-04-08 13:32:22 -0600

    Added a footer component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
+
   <div id="app" class="bg-gray-200 w-full h-screen">
     <Navigation/>
-    <router-view class="container mx-auto"/>
+    <router-view class="container mx-auto" :user="user"/>
+    <Footer/>
   </div>
-</template>
-<script>
 
+</template>
+
+
+<script>
 import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
+// import Firebase from 'firebase'
+import db from './db.js'
 
 export default {
-  name: 'app',
+  name: 'App',
+  data: function(){
+      return{
+          user: null
+      };
+  },
+  mounted(){
+      db.collection("users")
+      .doc("uIVgChm4DsIqGgjx1oB4")
+      .get()
+      .then(snapshot => {
+          this.user = snapshot.data().name;
+      })
+  },
   components: {
-    Navigation
+    Navigation,
+    Footer
   }
-}
+};
 </script>
